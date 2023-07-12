b12b01d0ab8049e9032a30e17a434bac42943e90
Davy de Lima Carvalho <59965417+DavyCarvalho@users.noreply.github.com>
2023-07-11 23:59:16 -0300

    Update login.js

--- my_contacts_frontend/src/pages/login.js
+++ my_contacts_frontend/src/pages/login.js
@@ -82,7 +82,7 @@ export default function Login() {
         <button
           type="submit"
           className="btn btn-dark button"
-          confirmButtonDisabledd={confirmButtonDisabled}
+          disabled={confirmButtonDisabled}
           onClick={ callApiToAuthenticateUser }
         >
           Entrar
